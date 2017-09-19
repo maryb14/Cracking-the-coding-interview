4fe860e7ee5f62e68dfb53f5cc8bf862183ed5f1
Marilena <mary_bescuca@yahoo.com>
2017-09-19 22:59:44 +0300

    uncommented tests

--- test.cpp
+++ test.cpp
@@ -38,39 +38,39 @@ TEST(fourth_test_ch1q5, Ch1Q5) {
 	EXPECT_EQ(solToCh1Q5.areStringsOneEditAway("pale", "bake"), false);
 }
 
-Sorting <int> IntSorting;
+Sorting <int> IntSortingMerge;
 
-TEST(int_test, Sorting) {
+TEST(int_test_merge, Sorting) {
 	vector <int> input_vector = { 5, 3, 1, 4, 2 };
 	vector <int> result_vector = { 1 , 2, 3, 4, 5 };
-	EXPECT_EQ(IntSorting.merge_sort(input_vector), result_vector);
+	EXPECT_EQ(IntSortingMerge.merge_sort(input_vector), result_vector);
 }
 
-Sorting <double> FloatSorting;
+Sorting <double> FloatSortingMerge;
 
-TEST(float_test, Sorting) {
+TEST(float_test_merge, Sorting) {
 	vector <double> input_vector = { 5.25, 3.61, 1.25, 4.369, 2.0005 };
 	vector <double> result_vector = { 1.25 , 2.0005, 3.61, 4.369, 5.25 };
-	EXPECT_EQ(FloatSorting.merge_sort(input_vector), result_vector);
+	EXPECT_EQ(FloatSortingMerge.merge_sort(input_vector), result_vector);
 }
 
-//Sorting <int> IntSorting;
-//
-//TEST(Sorting, int_test) {
-//	vector <int> input_vector = { 5, 3, 1, 4, 2 };
-//	vector <int> result_vector = { 1 , 2, 3, 4, 5 };
-//	IntSorting.quick_sort(input_vector, 0, input_vector.size());
-//	EXPECT_EQ(input_vector, result_vector);
-//}
-//
-//Sorting <double> FloatSorting;
-//
-//TEST(Sorting, float_test) {
-//	vector <double> input_vector = { 5.25, 3.61, 1.25, 4.369, 2.0005 };
-//	vector <double> result_vector = { 1.25 , 2.0005, 3.61, 4.369, 5.25 };
-//	FloatSorting.quick_sort(input_vector, 0, input_vector.size());
-//	EXPECT_EQ(input_vector, result_vector);
-//}
+Sorting <int> IntSortingQuick;
+
+TEST(int_test_quick, Sorting) {
+	vector <int> input_vector = { 5, 3, 1, 4, 2 };
+	vector <int> result_vector = { 1 , 2, 3, 4, 5 };
+	IntSortingQuick.quick_sort(input_vector, 0, input_vector.size());
+	EXPECT_EQ(input_vector, result_vector);
+}
+
+Sorting <double> FloatSortingQuick;
+
+TEST(float_test_quick, Sorting) {
+	vector <double> input_vector = { 5.25, 3.61, 1.25, 4.369, 2.0005 };
+	vector <double> result_vector = { 1.25 , 2.0005, 3.61, 4.369, 5.25 };
+	FloatSortingQuick.quick_sort(input_vector, 0, input_vector.size());
+	EXPECT_EQ(input_vector, result_vector);
+}
 
 
 
